87d4e9673dddd45ef86a67dfe0bae418666c42ed
Teddy Luben <teddyluben@Teddys-MacBook-Pro.local>
2020-05-01 11:20:37 -0600

    styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,15 @@
 import React from "react"
 
+const style = {
+    color:'blue'
+}
+
 function Card({project}) {
     return (
         <div className="card" style={{width:"18rem"}}>
             <img className="card-img-top" src={project.img} alt="hi" />
             <div className="card-body">
-    <h5 className="card-title">{project.title}</h5>
+                <h5 style={style} className="card-title">{project.title}</h5>
                 <p className="card-text">Description Goes Here</p>
                 <a href={project.deployed} className="btn btn-primary">Deployed</a>
                 <a href={project.github} className="btn btn-primary">Github</a>
