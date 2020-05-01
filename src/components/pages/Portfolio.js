72226fa6de312c88153a9617cc540aaadc891c8b
Teddy Luben <teddyluben@Teddys-MacBook-Pro.local>
2020-05-01 11:50:45 -0600

    more styling updates

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Card from "../Card";
 
+const cardstyle = {
+  border: "15px"
+}
+
 function Portfolio({projects}) {
   return (
     <div className="container className-centered">
       <h1 className="text-center">Teddy's Portfolio</h1>
-      <div className ="row">
+      <div style={cardstyle} className ="row">
         {projects.map(project =>(
           <Card project={project}/>
         ))}
