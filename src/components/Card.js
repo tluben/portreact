dbf66174d60b07e111ff8de8db00d974b9d8389b
Teddy Luben <teddyluben@Teddys-MacBook-Pro.local>
2020-05-04 11:04:42 -0600

    reactified codepenn starter code

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,15 +16,36 @@ const img ={
     paddingTop: "20px"
 }
 
+const card_title ={
+    textAlign: "center",
+    //borderRadius: 0px 0px 40px 40px;
+
+    fontFamily: "sans-serif",
+    fontWeight: "bold",
+    fontSize: "30px",
+    marginTop: "80px",
+    height: "40px"
+  }
+
 function Card({project}) {
+    // return (
+    //     <div style={card} className="card">
+    //         <img style={img} className="card-img-top" src={project.img} alt="hi" />
+    //         <div className="card-body">
+    //             <h5 style={header} className="card-title">{project.title}</h5>
+    //             <p className="card-text">Description Goes Here</p>
+    //             <a href={project.deployed} className="btn btn-primary">Deployed</a>
+    //             <a href={project.github} className="btn btn-primary">Github</a>
+    //         </div>
+    //     </div>
+    // )
     return (
-        <div style={card} className="card">
-            <img style={img} className="card-img-top" src={project.img} alt="hi" />
-            <div className="card-body">
-                <h5 style={header} className="card-title">{project.title}</h5>
-                <p className="card-text">Description Goes Here</p>
-                <a href={project.deployed} className="btn btn-primary">Deployed</a>
-                <a href={project.github} className="btn btn-primary">Github</a>
+        <div class="card 1">
+            <div className="card_image"> 
+                <img src="https://i.redd.it/b3esnz5ra34y.jpg" alt=""/> 
+            </div>
+            <div style={card_title}>
+                <p>Card Title</p>
             </div>
         </div>
     )
